d458279bd40f518d93e3635a4b3fa5598c4ead46
Andrew Cholakian <andrew@andrewvc.com>
2017-08-15 22:20:19 -0500

    Revert so we can do initial import as a PR

--- lib/logstash_registry.rb
+++
@@ -1,6 +0,0 @@
-require 'logstash/integrations/internal'
-
-LogStash::Integrations::Internal.start!
-
-LogStash::PLUGIN_REGISTRY.add(:input, "internal", LogStash::Integrations::Internal::Input)
-LogStash::PLUGIN_REGISTRY.add(:output, "internal", LogStash::Integrations::Internal::Output)
