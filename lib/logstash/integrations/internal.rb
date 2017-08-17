cacfaaa1903c84662717751bb7793b253e3a2bcb
Andrew Cholakian <andrew@andrewvc.com>
2017-08-16 22:28:55 -0500

    Added specs and other stuff

--- lib/logstash/integrations/internal.rb
+++ lib/logstash/integrations/internal.rb
@@ -1,21 +1,24 @@
 module LogStash; module Integrations; module Internal
   include ::LogStash::Util::Loggable
-
-  def self.start!
-    @inputs = java.util.concurrent.ConcurrentHashMap.new
-  end
+  
+  INPUTS = java.util.concurrent.ConcurrentHashMap.new()
 
   def self.addresses_by_run_state
-    result = {:running => [], :not_running => {}}
-    @inputs.forEach do |address, input| 
+    result = {:running => [], :not_running => []}
+    INPUTS.forEach do |address, input| 
       key = input.running? ? :running : :not_running
       result[key] << address
     end
     result
   end
 
+  # Only really useful for tests
+  def self.reset!
+    INPUTS.clear
+  end
+
   def self.send_to(address, events)
-    input = @inputs.get(address);
+    input = INPUTS.get(address);
     # Internal receive returns a boolean indicating whether the receive was successful or not
     # If the result is false then the sender will retry.
     # If this were not here we'd have a race where an input could be stopped after the CHM.get above
@@ -23,20 +26,18 @@ module LogStash; module Integrations; module Internal
     # You might think this would be solvable in a simpler way using CHM.compute {|address, input| input.internal_receive(events) }
     # but that would be problematic since `internal_receive` blocks indefinitely on pipeline backpressure
     # this is much more dependable
-    if input
-      return input.internal_receive(events)
-    end
+    return input && input.internal_receive(events)
   end
 
   # Return true if nothing was listening previously
   def self.listen(address, internal_input)
-    mapped_input = @inputs.putIfAbsent(address, internal_input).nil?
+    mapped_input = INPUTS.putIfAbsent(address, internal_input).nil?
     return mapped_input == internal_input
   end
 
   # Return true if the input was actually listening
   def self.unlisten(address, internal_input)
-    @inputs.remove(address, internal_input)
+    INPUTS.remove(address, internal_input)
   end
 
   class Input < ::LogStash::Inputs::Base
@@ -45,23 +46,23 @@ module LogStash; module Integrations; module Internal
     config :address, :validate => :string, :required => true
 
     def register
+      # May as well set this up here, writers won't do anything until
+      # @running is set to false
       @running = java.util.concurrent.atomic.AtomicBoolean.new(false)
-    end
-
-    def running?
-      @running.get()
+      Internal.listen(@address, self)
     end
 
     def run(queue)
       @queue = queue
-      Internal.listen(@address, self)
-
-      # Now that the listener is set up we can activate this
       @running.set(true)
 
       while @running.get()
         sleep 0.5
       end
+    end
+
+    def running?
+      @running.get()
     end
 
     # Returns false if the receive failed due to a stopping input
@@ -80,7 +81,7 @@ module LogStash; module Integrations; module Internal
 
     def stop
       # We stop receiving events before we unlisten to prevent races
-      @running.set(false)
+      @running.set(false) if @running # If register wasn't yet called, no @running!
       Internal.unlisten(@address, self)
     end
   end
@@ -91,16 +92,18 @@ module LogStash; module Integrations; module Internal
     config :send_to, :validate => :string, :required => true, :list => true
 
     def register
-      # Noop, needed to conform to plugin API
+      # noop, needed to obey plugin API
     end
 
     def multi_receive(events)
       @send_to.each do |address|
         while !Internal.send_to(address, events)
           sleep 1
-          @logger.info("Internal output to address waiting for listener to start",
+          @logger.info(
+            "Internal output to address waiting for listener to start",
             :destination_address => address,
-            :registered_addresses => Internal.addresses_by_run_state)
+            :registered_addresses => Internal.addresses_by_run_state
+          )
         end
       end
     end
